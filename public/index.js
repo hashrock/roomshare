b8eb69c6e43579b340c68e67644cd7c00897115b
hashrock <hashrock@users.noreply.github.com>
2019-08-18 13:49:29 +0900

    style: format

--- public/index.js
+++ public/index.js
@@ -1,29 +1,31 @@
-const game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+const game = new Phaser.Game(800, 600, Phaser.AUTO, "phaser-example", {
+  preload: preload,
+  create: create,
+  update: update,
+  render: render
+});
 
 function preload() {
-  game.load.tilemap('map', 'init.csv', null, Phaser.Tilemap.CSV);
-  game.load.image('tiles', 'init.png');
+  game.load.tilemap("map", "init.csv", null, Phaser.Tilemap.CSV);
+  game.load.image("tiles", "init.png");
 }
 
 //mapとlayerはeditorからも書き換える
 let map;
 let layer;
 
-
 let marker;
 let currentTile;
 let cursors;
 
-
-var starCountRef = firebase.database().ref('map');
-starCountRef.on('value', function(snapshot) {
-  console.log(snapshot.val())
+var starCountRef = firebase.database().ref("map");
+starCountRef.on("value", function(snapshot) {
+  console.log(snapshot.val());
 });
 
-
 function create() {
-  map = game.add.tilemap('map', 32, 32);
-  map.addTilesetImage('tiles');
+  map = game.add.tilemap("map", 32, 32);
+  map.addTilesetImage("tiles");
   layer = map.createLayer(0);
   //currentTile = map.getTile(2, 3);
   currentTile = 0;
@@ -34,41 +36,45 @@ function create() {
   marker.drawRect(0, 0, 32, 32);
 
   cursors = game.input.keyboard.createCursorKeys();
-
 }
 
 function update() {
-
   marker.x = layer.getTileX(game.input.activePointer.worldX) * 32;
   marker.y = layer.getTileY(game.input.activePointer.worldY) * 32;
 
   if (game.input.mousePointer.isDown) {
     if (game.input.keyboard.isDown(Phaser.Keyboard.SHIFT)) {
-      currentTile = map.getTile(layer.getTileX(marker.x), layer.getTileY(marker.y));
-    }
-    else {
-      if (map.getTile(layer.getTileX(marker.x), layer.getTileY(marker.y)).index != currentTile.index) {
-        map.putTile(currentTile, layer.getTileX(marker.x), layer.getTileY(marker.y));
+      currentTile = map.getTile(
+        layer.getTileX(marker.x),
+        layer.getTileY(marker.y)
+      );
+    } else {
+      if (
+        map.getTile(layer.getTileX(marker.x), layer.getTileY(marker.y)).index !=
+        currentTile.index
+      ) {
+        map.putTile(
+          currentTile,
+          layer.getTileX(marker.x),
+          layer.getTileY(marker.y)
+        );
       }
     }
   }
 
   if (cursors.left.isDown) {
     game.camera.x -= 4;
-  }
-  else if (cursors.right.isDown) {
+  } else if (cursors.right.isDown) {
     game.camera.x += 4;
   }
 
   if (cursors.up.isDown) {
     game.camera.y -= 4;
-  }
-  else if (cursors.down.isDown) {
+  } else if (cursors.down.isDown) {
     game.camera.y += 4;
   }
-
 }
 
 function render() {
-  game.debug.text('マップチップかけるよー', 32, 32, '#efefef');
+  game.debug.text("マップチップかけるよー", 32, 32, "#efefef");
 }
